9626bdf3c6868a07a225149a81a6d1e31fafbb3d
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-06-28 20:48:29 +0300

    Fix uninstall.sh script

--- uninstall.sh
+++ uninstall.sh
@@ -3,8 +3,11 @@
 set -e
 
 prefix="${XDG_DATA_HOME:-$HOME/.local/share}"
-krunner_dbusdir="$prefix/krunner/dbusplugins"
+krunner_dir="$prefix/krunner"
+krunner_dbusdir="$krunner_dir/dbusplugins"
 
 rm $prefix/dbus-1/services/org.kde.krunner_emacs.service
 rm $krunner_dbusdir/krunner_emacs.desktop
+rm $krunner_dir/krunner_emacs.el
+
 kquitapp6 krunner
